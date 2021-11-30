0d01dc5b4ffe0b07c6bd97b333e4990d9e7766cd
Raynard Dodzi Helegah <dodzireynard@gmail.com>
2021-11-30 13:09:20 +0000

    update

--- app/UgMap.java
+++ app/UgMap.java
@@ -8,7 +8,7 @@ public class UgMap extends Canvas{
     public void paint(Graphics g) { 
         g.setColor(Color.CYAN); 
         Toolkit t=Toolkit.getDefaultToolkit();  
-        Image i = t.getImage("/Users/hrd/Documents/UG/2020 2021/Sem 2/DCIT 204/code/ug-map/app/map_info.png");  
+        Image i = t.getImage("/Users/hrd/Documents/UG/2020 2021/Sem 2/DCIT 204/code/ug-map/app/assets/map_info.png");  
         g.drawImage(i,10, 10, this);  
     } 
 
